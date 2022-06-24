d9c6b5625b422d201a674e4f724e30ba815b3b51
VyTsan <tosanvy@gmail.com>
2022-06-24 17:36:49 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+const divs = [1,2,3,4,5,6,7];
 
 function App() {
+  // Xử lý logic bài 2
+  const [checkedDiv,setCheckedDiv] = useState(divs[0])
+  const handleNext = () => {
+    setCheckedDiv((prev) => (prev===divs[divs.length-1]) ? divs[0] : prev + 1);
+  }
+  // Xử lý logic bài 3
+  const [circleMode,setCircleMode] = useState('false')
+  const handleChangeShape = () => {
+    setCircleMode((prev) => !prev);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>BÀI TẬP SỐ 3</h1>
+      <h2>Bài 3.1</h2>
+      <div>
+        {divs.filter((num) => num!==7).map((filteredNum,index) => (
+          <div key={`bai1-${index}`} className={`square ${(filteredNum%2===0) && 'circle'}`}></div>
+        ))}
+      </div>
+      <hr/>
+
+      <h2>Bài 3.2</h2>
+      <div>
+        {divs.map((num,index) => (
+          <div key={`bai2-${index}`} className={`square ${(num===checkedDiv) && 'colored'}`}></div>
+        ))}
+      </div>
+      <button onClick={handleNext}>Next</button>
+      <hr/>
+      
+      <h2>Bài 3.3</h2>
+      <div className={`square ${circleMode && 'circle'}`}></div> <br/>
+      <button onClick={handleChangeShape}>Change Shape</button>
+
     </div>
   );
 }
